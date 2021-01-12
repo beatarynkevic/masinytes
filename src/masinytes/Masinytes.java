ada534e68f6f5558ac31e06dcf04157b52fe7cce
Kompiuteris <Kompiuteris@DESKTOP-6C0UQ0S>
2021-01-12 19:07:01 +0200

    Initial code

---
+++ src/masinytes/Masinytes.java
@@ -0,0 +1,27 @@
+/*
+*0 0 0 0 0
+*iteracijos metu random kiekviena pavaziuoja
+*1-10 Math.random
+*2 5 3 1
+*5 6 8 7
+*7 9 12 11
+
+*bent viena turi 10km nuvaziuoti
+*atspausdina atstuma maziejimo tvarka
+ */
+package masinytes;
+
+/**
+ *
+ * @author Kompiuteris
+ */
+public class Masinytes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] masinos = new int[8];
+    }
+    
+}
