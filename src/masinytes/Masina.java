e1124b2568922351628b27aadc8e7415ddd68b66
Kompiuteris <Kompiuteris@DESKTOP-6C0UQ0S>
2021-01-18 13:33:18 +0200

    be spoilerio

--- src/masinytes/Masina.java
+++ src/masinytes/Masina.java
@@ -1,19 +1,10 @@
 /*
-    void gazuok(int kiek)
-    void stabdyk(int kiek)
-    void vaziuok()
-
-    Jei sportine masina
-    50% kad pasikeis spoilerio pozicija
-
+//pries nusprendziat masina stbdys ar gazuos, reikia ptikrinti ar tai sportine masina
+    Jeigu Sportine masina: 50% kad pasikeis spoilerio pozicija
 
     30% stabdys 1-5
     50% gazuos 1-10
     20% nekeis greicio
-
-
-    SportineMasina
-
     gazuok() jei spoileris nuleistas - gazuoja 2x greiciau
     stabdyk() jei spoileris pakeltas - stabdo 2x greiciau
 
@@ -21,10 +12,72 @@
  */
 package masinytes;
 
-/**
- *
- * @author Kompiuteris
- */
 public class Masina {
+    private String pavadinimas;
+    public int maxGreitis;
+    public int kelias; //pirma masina turi 0
+    public int greitis;
+    
+    public Masina(String pavadinimas, int maxGreitis) {
+        this.pavadinimas = pavadinimas;
+        this.maxGreitis = maxGreitis;
+        this.kelias = 0;
+        this.greitis = 0;
+    }
+     public void gazuok() {
+         // padidinti greiti
+         //jei greitis > max greitis, greitis = max greitis.
+         //nuvaziuoti atstuma./issaugoti duomenis
+         this.greitis += Math.random() * 10 +1;
+         if (this.greitis > this.maxGreitis){
+             this.greitis = this.maxGreitis;
+         }
+         this.kelias += this.greitis;
+     }
+     
+     public void stabdyk() {
+         
+         this.greitis -= Math.random() * 5 + 1;
+         if(this.greitis < 0) {
+             this.greitis = 0;
+         }
+         this.kelias += this.greitis;
+     }
+     
+     public void vaziuok() { //kelias padideja ant tiek koks masinos gretiis x = greitis - kelias, kelias+= x
+         this.kelias += this.greitis;
+         if(this.greitis == 0) {
+             this.gazuok();
+         }
+     }
+     
+     public void nuspreskKadaryt() {
+         double mr = Math.random();
+                if(mr <= 0.2) {
+//                    System.out.println("20% tikimybes");
+                    this.vaziuok();
+                    //nekeis greicio
+                }
+                if(mr > 0.2 && mr <= 0.5) {
+//                    System.out.println("30% tikimybes");
+                    this.stabdyk();
+                    //metodas stabdo
+                }
+                if(mr > 0.5) {
+//                    System.out.println("50% tikimybes");
+                    this.gazuok();
+                    //metodas gazuoja
+                }
+     }
+
+    public int getKelias() {
+        return kelias;
+    }
+
+    public void setKelias(int kelias) {
+        this.kelias = kelias;
+    }
+    
+  
     
 }
--- src/masinytes/Masinytes.java
+++ src/masinytes/Masinytes.java
@@ -14,56 +14,69 @@ package masinytes;
 public class Masinytes {
 
     public static void main(String[] args) {
+        Masina[] race = {           //kiekviena masina, tai objektas
+            new Masina("Pirma", 140),
+            new Masina("Antra", 160),
+            new Masina("Trecia", 180),
+            new Masina("Ketvirta", 220),
+            //new SportineMasina("sp1", 300),
+            //new SportineMasina("sp1", 300),
+        };
+        
         int[] cars = new int[8];
         int interm = 100;
         boolean doRace = true;
-        
         while (doRace) {
-            for (int i = 0; i < cars.length; i++) {     //visos masinos pavaziavo
-                cars[i] += Math.random() * 10 + 1;
+//            for (int a = 0; a < 10; a++) { 
+            for (int i = 0; i < race.length; i++) {     //visos masinos pavaziavo
+                race[i].nuspreskKadaryt();
+  
             }
             
             boolean printInterm = false;        //komentatorius
             int intermWinner = 0;
             int intermWinnerKm = 0;
-            for (int i = 0; i < cars.length; i++) {
-                if (cars[i] > intermWinnerKm) {
-                    intermWinner = i + 1;
-                    intermWinnerKm = cars[i];
+            for (int i = 0; i < race.length; i++) {
+                if (race[i].getKelias() > intermWinnerKm) {
+                    intermWinner = i + 1; //kuris automobilis
+                    intermWinnerKm = race[i].getKelias();
                 }
-                if (cars[i] >= interm) {
+                if (race[i].getKelias() >= interm) {
                     printInterm = true;
                     interm += 100;
                 }
             }
             if (printInterm) {
-                for (int i = 0; i < cars.length; i++) {
-                    System.out.print(cars[i] + "\t");
+                for (int i = 0; i < race.length; i++) {
+                    System.out.print(race[i].getKelias() + "\t");
                 }
                 System.out.println();
                 System.out.println(intermWinner + " automobilis nuvaziavo: " + intermWinnerKm);
                 System.out.println();
             }
-            for (int i = 0; i < cars.length; i++) {     //tikrinimas
-                if (cars[i] >= 1000) {
+            for (int i = 0; i < race.length; i++) {     //tikrinimas
+                if (race[i].getKelias() >= 1000) {
                     doRace = false;
                     break;
                 }
             }
         }
-        
-        for (int i = 0; i < cars.length - 1; i++) {     //rusiavimas
-            for (int j = i + 1; j < cars.length; j++) {
-                if (cars[i] < cars[j]) {
-                    int tmp = cars[i];
-                    cars[i] = cars[j];
-                    cars[j] = tmp;
+        for (int i = 0; i < race.length - 1; i++) {     //rusiavimas
+            for (int j = i + 1; j < race.length; j++) {
+                if (race[i].getKelias() < race[j].getKelias()) {
+                    Masina tmp = race[j];
+                    race[j] = race[i];
+                    race[i] = tmp;
                 }
             }
         }
-        
-        for (int i = 0; i < cars.length; i++) {
-            System.out.println(cars[i]);
+        for (int i = 0; i < race.length; i++) {
+            System.out.println(race[i].getKelias());
         }
     }
+    /*
+        int mantoAmzius = 286154641;
+        String mA = mantoAmzius+"";
+        System.out.println(mA.charAt(mA.length()-3)); 
+    */
 }
